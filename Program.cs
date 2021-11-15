d090b4eb001ff94e36a3b330d90aed375396032b
SalamBhai <saulasheriffdeen@gmail.com>
2021-11-15 13:27:04 +0100

    I created a new c# Project and erased the default console text then issued the program to welcome Salam Bhai

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
